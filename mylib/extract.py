c654e9288da9be9925ba3b0e534312e619be3bad
simrunsharma <141798228+simrunsharma@users.noreply.github.com>
2023-10-15 03:06:29 +0000

    message

--- mylib/extract.py
+++ mylib/extract.py
@@ -9,17 +9,26 @@ import pandas as pd
 
 
 def extract(
-    url="https://raw.githubusercontent.com/nogibjj/Simrun_sqlite-lab/main/data/Jeopardy.csv",
-    file_path="data/Jeopardy.csv",
+    url_1="https://raw.githubusercontent.com/nogibjj/Simrun_sqlite-lab/main/data/Jeopardy.csv",
+    file_path="data/Jeopardy.csv", url_2 = "https://github.com/nogibjj/Simrun_External_Database_SQL/raw/main/data/Jeopardy2.csv",
+    file_path_2 = "data/Jeopardy2.csv"
 ):
     """ "Extract a url to a file path"""
-    with requests.get(url) as r:
+    with requests.get(url_1) as r:
         with open(file_path, "wb") as f:
             f.write(r.content)
 
     df1 = pd.read_csv(file_path, delimiter=",", skiprows=1)
     print(df1.head())
-    return df1, file_path
+
+    with requests.get(url_2) as r:
+        with open(file_path_2, "wb") as f:
+            f.write(r.content)
+    
+    df2 = pd.read_csv(file_path_2, delimiter=",", skiprows=1)
+    print(df2.head())
+
+    return df1, df2
 
 
 extract()
