247040c449acbae93053dd9c79a05195c046bb31
simrunsharma <141798228+simrunsharma@users.noreply.github.com>
2023-10-15 20:12:30 +0000

    try

--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -12,41 +12,62 @@ from dotenv import load_dotenv
 from delta.tables import DeltaTable
 
 
-def load(dataset_1="data/Jeopardy.csv", dataset_2="data/Jeopardy2.csv"):
-    """Loads the dataset from temporary csv from data/Jeopardy.csv and into a Databricks SQL database"""
+def load(dataset="data/Jeopardy.csv", dataset2 = "data/Jeopardy2.csv""):
+    """Loads the dataset from temporary csv from data/Jeopardy.csv and into a Databricks remote database"""
 
-    df1 = pd.read_csv(dataset_1, delimiter=",", skiprows=1)
-    df2 = pd.read_csv(dataset_2, delimiter=",", skiprows=1)
+    df1 = pd.read_csv(dataset, delimiter=",", skiprows=1)
+    df2 = pd.read_csv(dataset2, delimiter=",", skiprows=1)
 
-    load_dotenv(dotenv_path='keys.env')
+    load_dotenv()
     host = os.getenv("DB_HOST")
     token = os.getenv("DB_TOKEN")
     path = os.getenv("DB_PATH")
 
     with sql.connect(
-        server_hostname=host,
+        server_hostname = host,
         http_path=path,
         access_token=token,
     ) as connection:
         c = connection.cursor()
+        c.execute("SHOW TABLES FROM default")
+        tables = c.fetchall()
 
-        # Create jeopardy table
-        c.execute('''
-    CREATE TABLE IF NOT EXISTS jeopardy (
-        `Show Number` STRING,
-        `Air Date` STRING,
-        `Round` STRING,
-        `Category` STRING,
-        `Value` STRING,
-        `Question` STRING,
-        `Answer` STRING
-    )
-''')
-# Assuming 'spark' is your SparkSession
-delta = DeltaTable.forPath(spark, "path_to_table")
-delta.upgradeTableProtocol(1, 3)  # Upgrades to readerVersion=1, writerVersion=3
+        for table in tables:
+            print(table)
+            table_name = table.tableName
+            c.execute(f"DROP TABLE IF EXISTS {table_name}")
 
-print("jeopardy table created successfully.")
+        c.execute("SHOW TABLES FROM default LIKE 'jeopardy'")
+        result = c.fetchall()
+        print(f"tables : {result}")
+        if not result:
+            c.execute(
+                """ 
+                    CREATE TABLE IF NOT EXISTS jeopardy (
+                        Show Number,
+                        Air Date,
+                        Round,
+                        Category,
+                        Value,
+                        Question,
+                        Answer
+                        )
+                    """
+                )
+            for _, row in df1.iterrows():
+                new_row = []
+                for r in row:
+                    if r is None:
+                        new_row.append("Null")
+                    else:
+                        new_row.append(r)
+                print(new_row)
+                convert = (_,) + tuple(new_row)
+                c.execute(f"INSERT INTO jeopardy VALUES {convert}")
+        full_results = c.fetchall()
+        print(full_results)
+        c.close()
+    return "done"
 
 
 if __name__ == "__main__":
